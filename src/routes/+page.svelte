5fda998891b3c743142d0fdfea5e106230a9b726
Michael Townsend <michael@doublespeakgames.com>
2026-07-10 19:47:27 -0400

    user register/login/logout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import { m } from '$lib/paraglide/messages';
+  import type { PageProps } from './$types';
+  let { data }: PageProps = $props();
+  let name = $derived(data.user?.name ?? 'Anon');
+</script>
+
+<p>{m.hello_world({ name })}</p>
+{#if data.user}
+  <form method="post" action="?/signOut" use:enhance>
+    <button class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+      >Sign out</button
+    >
+  </form>
+{/if}
